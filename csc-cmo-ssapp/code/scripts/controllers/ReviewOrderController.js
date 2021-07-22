02a3100edeecdbede1d5e3e6ac94cb7ccfe5393f
Dune <fotisn@gmail.com>
2021-07-22 16:37:39 +0300

    fix for review order

--- csc-cmo-ssapp/code/scripts/controllers/ReviewOrderController.js
+++ csc-cmo-ssapp/code/scripts/controllers/ReviewOrderController.js
@@ -1,72 +1,69 @@
 const { WebcController } = WebCardinal.controllers;
-const cscServices = require("csc-services")
-const OrdersService  = cscServices.OrderService;
+const cscServices = require('csc-services');
+const OrdersService = cscServices.OrderService;
 const eventBusService = cscServices.EventBusService;
-const Topics  = cscServices.constants.Topics;
+const Topics = cscServices.constants.Topics;
 const viewModelResolver = cscServices.viewModelResolver;
 
 export default class ReviewOrderController extends WebcController {
-
     constructor(...props) {
-
         super(...props);
-        let  order  = this.history.location.state.order;
+        let order = this.history.location.state.order;
 
         this.ordersService = new OrdersService(this.DSUStorage);
 
         this.model = {
             wizard_form: [
-                { id: "wizard_form_step_1" , holder_id: "wizard_form_step_1_holder", name : "Order Details" , visible: true , validated: false },
-                { id: "wizard_form_step_2" , holder_id: "wizard_form_step_2_holder", name : "Attach Documents" , visible: false , validated: false},
-                { id: "wizard_form_step_3" , holder_id: "wizard_form_step_3_holder", name : "Comments" , visible: false, validated: false},
-                { id: "wizard_form_step_4" , holder_id: "wizard_form_step_4_holder", name : "Confirmation" , visible: false, validated: false}
+                { id: 'wizard_form_step_1', holder_id: 'wizard_form_step_1_holder', name: 'Order Details', visible: true, validated: false },
+                { id: 'wizard_form_step_2', holder_id: 'wizard_form_step_2_holder', name: 'Attach Documents', visible: false, validated: false },
+                { id: 'wizard_form_step_3', holder_id: 'wizard_form_step_3_holder', name: 'Comments', visible: false, validated: false },
+                { id: 'wizard_form_step_4', holder_id: 'wizard_form_step_4_holder', name: 'Confirmation', visible: false, validated: false },
             ],
-            wizard_form_navigation:[
-                { id: "from_step_1_to_2", name: "Next", visible: true , validated: false },
-                { id: "from_step_2_to_1", name: "Previous", visible: true , validated: false },
-                { id: "from_step_2_to_3", name: "Next", visible: true , validated: false },
-                { id: "from_step_3_to_2", name: "Previous", visible: true , validated: false },
-                { id: "from_step_3_to_4", name: "Next", visible: true , validated: false },
+            wizard_form_navigation: [
+                { id: 'from_step_1_to_2', name: 'Next', visible: true, validated: false },
+                { id: 'from_step_2_to_1', name: 'Previous', visible: true, validated: false },
+                { id: 'from_step_2_to_3', name: 'Next', visible: true, validated: false },
+                { id: 'from_step_3_to_2', name: 'Previous', visible: true, validated: false },
+                { id: 'from_step_3_to_4', name: 'Next', visible: true, validated: false },
             ],
-            form: viewModelResolver("order").form,
-            allComments:JSON.parse(JSON.stringify(order.comments)),
-            order:order
+            form: viewModelResolver('order').form,
+            allComments: JSON.parse(JSON.stringify(order.comments)),
+            order: order,
         };
 
-        const disabledInputs  = ["sponsor_id","site_region_id","site_country"];
+        const disabledInputs = ['sponsor_id', 'site_region_id', 'site_country'];
 
-        disabledInputs.forEach((input)=>{
+        disabledInputs.forEach((input) => {
             this.model.form.inputs[input].disabled = true;
         });
 
         this.model.addExpression(
-            "hasComments",
+            'hasComments',
             () => {
-                return this.model.form.comments.length >=1;
+                return this.model.form.comments.length >= 1;
             },
-            "form.comments.length"
+            'form.comments.length'
         );
 
-        this.model.onChange("form.inputs.add_comment.value",()=>{
+        this.model.onChange('form.inputs.add_comment.value', () => {
             let comment = this.model.form.inputs.add_comment.value;
-            this.model.allComments = [...this.model.form.comments]
-            if(comment){
+            this.model.allComments = [...this.model.form.comments];
+            if (comment) {
                 this.model.allComments.push({
-                    entity:"CMO",
-                    date:new Date().toLocaleString(),
-                    comment:comment
-                })
+                    entity: 'CMO',
+                    date: new Date().toLocaleString(),
+                    comment: comment,
+                });
             }
-        })
+        });
 
         this.on('add-file', (event) => {
-
             const files = event.data;
 
-            if(files){
-                files.forEach( (file) => {
-                    this.model.form.documents.push({ name: file.name , attached_by : "Novartis" , date: new Date().toLocaleString() , link : "" , file: file});
-                })
+            if (files) {
+                files.forEach((file) => {
+                    this.model.form.documents.push({ name: file.name, attached_by: 'Novartis', date: new Date().toLocaleString(), link: '', file: file });
+                });
             }
 
             if (event.data) this.docs = event.data;
@@ -94,59 +91,55 @@ export default class ReviewOrderController extends WebcController {
         //
         // },500);
 
-
         //When you click step 1
         this.onTagEvent('wizard_form_step_1', 'click', (e) => {
-            makeStepActive("wizard_form_step_1", "wizard_form_step_1_holder" , e);
+            makeStepActive('wizard_form_step_1', 'wizard_form_step_1_holder', e);
         });
 
         //When you click step 2
         this.onTagEvent('wizard_form_step_2', 'click', (e) => {
-            makeStepActive("wizard_form_step_2", "wizard_form_step_2_holder" , e);
+            makeStepActive('wizard_form_step_2', 'wizard_form_step_2_holder', e);
         });
 
         //When you click step 3
         this.onTagEvent('wizard_form_step_3', 'click', (e) => {
-            makeStepActive("wizard_form_step_3", "wizard_form_step_3_holder" , e);
+            makeStepActive('wizard_form_step_3', 'wizard_form_step_3_holder', e);
         });
 
         //When you click step 4
         this.onTagEvent('wizard_form_step_4', 'click', (e) => {
-            makeStepActive("wizard_form_step_4", "wizard_form_step_4_holder" , e);
+            makeStepActive('wizard_form_step_4', 'wizard_form_step_4_holder', e);
         });
-
 
         //STEP BUTTONS LOGIC
 
         //When you want to navigate from step 1 to step 2
         this.onTagEvent('from_step_1_to_2', 'click', (e) => {
-            makeStepActive("wizard_form_step_2", "wizard_form_step_2_holder" , e);
+            makeStepActive('wizard_form_step_2', 'wizard_form_step_2_holder', e);
         });
 
         //When you want to navigate from step 2 to step 1
         this.onTagEvent('from_step_2_to_1', 'click', (e) => {
-            makeStepActive("wizard_form_step_1", "wizard_form_step_1_holder" , e);
+            makeStepActive('wizard_form_step_1', 'wizard_form_step_1_holder', e);
         });
 
         //When you want to navigate from step 2 to step 3
         this.onTagEvent('from_step_2_to_3', 'click', (e) => {
-            makeStepActive("wizard_form_step_3", "wizard_form_step_3_holder" , e);
+            makeStepActive('wizard_form_step_3', 'wizard_form_step_3_holder', e);
         });
 
         //When you want to navigate from step 3 to step 2
         this.onTagEvent('from_step_3_to_2', 'click', (e) => {
-            makeStepActive("wizard_form_step_2", "wizard_form_step_2_holder" , e);
+            makeStepActive('wizard_form_step_2', 'wizard_form_step_2_holder', e);
         });
 
         //When you want to navigate from step 3 to step 2
         this.onTagEvent('from_step_3_to_4', 'click', (e) => {
-            makeStepActive("wizard_form_step_4", "wizard_form_step_4_holder" , e);
+            makeStepActive('wizard_form_step_4', 'wizard_form_step_4_holder', e);
         });
-
 
         //When you submit form
         this.onTagEvent('form_submit', 'click', (e) => {
-
             this.showErrorModal(
                 new Error(`Are you sure you want to submit the review?`), // An error or a string, it's your choice
                 'Submit Review',
@@ -156,58 +149,57 @@ export default class ReviewOrderController extends WebcController {
                     disableExpanding: true,
                     cancelButtonText: 'No',
                     confirmButtonText: 'Yes',
-                    id: 'error-modal'
+                    id: 'error-modal',
                 }
             );
-
-
         });
 
         const onSubmitYesResponse = async () => {
             const payload = {};
 
-            if(this.model.form){
-                if(this.model.form.inputs){
+            debugger;
+            if (this.model.form) {
+                if (this.model.form.inputs) {
                     let keys = Object.keys(this.model.form.inputs);
-                    if(keys){
-                        keys.forEach( (key) => {
-
-                            if(key === 'delivery_date'){
+                    if (keys) {
+                        keys.forEach((key) => {
+                            if (key === 'delivery_date') {
                                 payload['delivery_date'] = this.getDateTime();
-                            }else if(key === 'keep_between_temperature'){
+                            } else if (key === 'keep_between_temperature') {
                                 payload['keep_between_temperature'] = this.getTemperature();
-                            }else{
+                            } else {
                                 payload[key] = this.model.form.inputs[key].value;
                             }
-                        })
+                        });
                     }
                 }
 
-                if(this.model.form.documents){
-                    payload["files"] = [];
-                    this.model.form.documents.forEach ((doc) => {
-                        payload["files"].push(doc.file);
-                    })
+                if (this.model.form.documents) {
+                    payload['files'] = [];
+                    this.model.form.documents.forEach((doc) => {
+                        payload['files'].push(doc.file);
+                    });
                 }
 
-
-
-                console.log("SUBMIT : Payload: " , payload);
+                console.log('SUBMIT : Payload: ', payload);
                 const orders = await this.ordersService.getOrders();
 
-                let dbOrder = orders.find(_order=>_order.keySSI === order.keySSI);
-                const dsuOrder = await this.ordersService.getOrder(order.keySSI,dbOrder.documentsKeySSI)
+                let dbOrder = orders.find((_order) => _order.keySSI === order.keySSI);
+                const dsuOrder = await this.ordersService.getOrder(order.keySSI, dbOrder.documentsKeySSI);
                 console.log(dsuOrder);
-                await this.ordersService.updateOrder(payload);
-                await this.ordersService.finishReview(payload.files.filter(x => x !== undefined), [payload.add_comment], order.keySSI);
-                
+                // await this.ordersService.updateOrder(payload);
+                await this.ordersService.finishReview(
+                    payload.files.filter((x) => x !== undefined),
+                    [payload.add_comment],
+                    order.keySSI
+                );
+
                 eventBusService.emitEventListeners(Topics.RefreshNotifications, null);
             }
 
-
             this.createWebcModal({
-                template: "orderCreatedModal",
-                controller: "OrderCreatedModalController",
+                template: 'orderCreatedModal',
+                controller: 'OrderCreatedModalController',
                 disableBackdropClosing: false,
                 disableFooter: true,
                 disableHeader: true,
@@ -215,101 +207,90 @@ export default class ReviewOrderController extends WebcController {
                 disableClosing: true,
                 disableCancelButton: true,
                 expanded: false,
-                centered: true
+                centered: true,
             });
         };
-
 
         const onNoResponse = () => console.log('Why not?');
 
         //When you submit form
         this.onTagEvent('form_reset', 'click', (e) => {
-
             const payload = {};
 
-            if(this.model.form){
-                if(this.model.form.inputs){
+            if (this.model.form) {
+                if (this.model.form.inputs) {
                     let keys = Object.keys(this.model.form.inputs);
-                    if(keys){
-                        keys.forEach( (key) => {
-
-                            if(key === 'delivery_date'){
+                    if (keys) {
+                        keys.forEach((key) => {
+                            if (key === 'delivery_date') {
                                 payload['delivery_date'] = this.getDateTime();
-                            }else if(key === 'keep_between_temperature'){
+                            } else if (key === 'keep_between_temperature') {
                                 payload['keep_between_temperature'] = this.getTemperature();
-                            }else{
-                                this.model.form.inputs[key].value = "";
+                            } else {
+                                this.model.form.inputs[key].value = '';
                             }
-                        })
+                        });
                     }
                 }
             }
-            console.log("SUBMIT : Payload: " , payload);
-
+            console.log('SUBMIT : Payload: ', payload);
         });
 
-
         //Add active menu class to element
-        function makeStepActive( step_id, step_holder_id , e ) {
-
-            if(e){
-                e.wizard_form.forEach( (item) => {
-                    document.getElementById(item.id).classList.remove("new-order-wizard-active");
+        function makeStepActive(step_id, step_holder_id, e) {
+            if (e) {
+                e.wizard_form.forEach((item) => {
+                    document.getElementById(item.id).classList.remove('new-order-wizard-active');
                     hideStep(item.holder_id);
                 });
 
-                document.getElementById(step_id).classList.add("new-order-wizard-active");
+                document.getElementById(step_id).classList.add('new-order-wizard-active');
 
                 showStep(step_holder_id);
-
             }
         }
 
         //Remove active menu class to element
-        function makeMenuInActive( element ){
-            document.getElementById(element).classList.remove("dashboard-tab-active");
+        function makeMenuInActive(element) {
+            document.getElementById(element).classList.remove('dashboard-tab-active');
         }
 
-        function hideStep( item ){
+        function hideStep(item) {
             var el = document.getElementById(item);
-            el.classList.remove("show-step");
-            el.classList.add("hide-step");
+            el.classList.remove('show-step');
+            el.classList.add('hide-step');
         }
 
-        function showStep( item ){
+        function showStep(item) {
             var el = document.getElementById(item);
-            el.classList.remove("hide-step");
-            el.classList.add("show-step");
+            el.classList.remove('hide-step');
+            el.classList.add('show-step');
         }
-
-
-
     }
 
-     onChange(event, id ){
-         this.model.form.inputs[id].value = event.target.value;
+    onChange(event, id) {
+        this.model.form.inputs[id].value = event.target.value;
     }
 
-
-    updateDate(event){
+    updateDate(event) {
         const value = event.target.value;
         this.model.form.inputs.delivery_date.date.value = value;
     }
 
-    updateTime(event){
+    updateTime(event) {
         const value = event.target.value;
         this.model.form.inputs.delivery_date.time.value = value;
     }
 
-    getDateTime(){
-        return this.model.form.inputs.delivery_date.date.value + ' ' + this.model.form.inputs.delivery_date.time.value;
+    getDateTime() {
+        debugger;
+        return this.model.order.delivery_date.date + ' ' + this.model.order.delivery_date.time;
     }
 
-    getTemperature(){
+    getTemperature() {
         return {
-            min: this.model.form.inputs.keep_between_temperature.min.value,
-            max: this.model.form.inputs.keep_between_temperature.max.value
+            min: this.model.order.temperatures.min,
+            max: this.model.order.temperatures.max,
         };
     }
-
 }
