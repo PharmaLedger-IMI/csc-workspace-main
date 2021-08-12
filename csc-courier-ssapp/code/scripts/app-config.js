b104f6057125f83678ebee7427d4cac71a95111a
paiusCatalin <p.cata95@gmail.com>
2021-08-12 16:26:03 +0300

    [WIP] clean courier-ssapp and integrate the unification of the common controller

--- csc-courier-ssapp/code/scripts/app-config.js
+++ csc-courier-ssapp/code/scripts/app-config.js
@@ -3,18 +3,16 @@ const { addControllers } = WebCardinal.preload;
 const cscServices = require('csc-services');
 const ACTOR = cscServices.constants.Roles.Courier;
 
-// const TableTemplateController = cscServices.getController('TableTemplateController', ACTOR);
-// const SingleOrderController = cscServices.getController('SingleOrderController', ACTOR);
-// const HistoryModalController = cscServices.getController('HistoryModalController', ACTOR);
-// const HeaderController = cscServices.getController('HeaderController', ACTOR);
+const TableTemplateController = cscServices.getController('TableTemplateController', ACTOR);
+const HistoryModalController = cscServices.getController('HistoryModalController', ACTOR);
+const HeaderController = cscServices.getController('HeaderController', ACTOR);
 const NotificationsController = cscServices.getController('NotificationsController', ACTOR);
 const DashboardMenuController = cscServices.getController('DashboardMenuController', ACTOR);
 
 addControllers({
-	// TableTemplateController,
-	// SingleOrderController,
-	// HistoryModalController,
-	// HeaderController,
+	TableTemplateController,
+	HistoryModalController,
+	HeaderController,
 	NotificationsController,
 	DashboardMenuController
 });
