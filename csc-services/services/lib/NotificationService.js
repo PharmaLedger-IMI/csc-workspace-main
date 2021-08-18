b9baabf1d5b30a373110aac7f826fb94d336e705
Mahapatra <MAHAPAR2@novartis.net>
2021-08-18 12:37:56 +0530

    notification modifed

--- csc-services/services/lib/NotificationService.js
+++ csc-services/services/lib/NotificationService.js
@@ -1,37 +1,40 @@
-const getSharedStorage  = require('./SharedDBStorageService.js').getSharedStorage;
+const getSharedStorage = require('./SharedDBStorageService.js').getSharedStorage;
 
 module.exports = class NotificationsService {
-  NOTIFICATIONS_TABLE = 'notifications';
+	NOTIFICATIONS_TABLE = 'notifications';
 
-  constructor(DSUStorage) {
-    this.storageService = getSharedStorage(DSUStorage);
-  }
+	constructor(DSUStorage) {
+		this.storageService = getSharedStorage(DSUStorage);
+	}
 
-  async getNumberOfUnreadNotifications() {
-    const notifications = await this.storageService.filter(this.NOTIFICATIONS_TABLE);
-    return notifications.filter((x) => !x.read).length;
-  }
+	async getNumberOfUnreadNotifications() {
+		const notifications = await this.storageService.filter(this.NOTIFICATIONS_TABLE);
+		return notifications.filter((x) => !x.read).length;
+	}
 
-  async getNotifications() {
-    const notifications = await this.storageService.filter(this.NOTIFICATIONS_TABLE);
-    return notifications;
-  }
+	async getNotifications() {
+		let notifications = await this.storageService.filter(this.NOTIFICATIONS_TABLE);
+		notifications = notifications.sort((a, b) => b.pk - a.pk);
+		return notifications;
+	}
 
-  async insertNotification(notification) {
-    const notifications = await this.storageService.filter(this.NOTIFICATIONS_TABLE);
-    const id = notifications.length || 0;
-    const newRecord = await this.storageService.insertRecord(this.NOTIFICATIONS_TABLE, id, notification);
-    return newRecord;
-  }
+	async insertNotification(notification) {
+		const notifications = await this.storageService.filter(this.NOTIFICATIONS_TABLE);
+		const id = notifications.length || 0;
+		const newRecord = await this.storageService.insertRecord(this.NOTIFICATIONS_TABLE, id, notification);
+		return newRecord;
+	}
 
-  async getNotification(id) {
-    const notification = await this.storageService.getRecord(this.NOTIFICATIONS_TABLE, id);
-    return notification;
-  }
+	async getNotification(id) {
+		const notification = await this.storageService.getRecord(this.NOTIFICATIONS_TABLE, id);
+		return notification;
+	}
 
-  async changeNotificationStatus(id) {
-    const notification = await this.getNotification(id);
-    await this.storageService.updateRecord(this.NOTIFICATIONS_TABLE, id, { ...notification, read: !notification.read });
-    return;
-  }
-}
+	async changeNotificationStatus(id) {
+		const notification = await this.getNotification(id);
+		await this.storageService.updateRecord(this.NOTIFICATIONS_TABLE, id, {
+			...notification,
+			read: !notification.read
+		});
+	}
+};
