0addc39417408f841c9b449a77883c997ff48347
abhayprasadnovartis <abhay.prasad_ext@novartis.com>
2021-08-31 12:16:10 +0530

    Merge conflict

--- csc-services/services/ShipmentsService.js
+++ csc-services/services/ShipmentsService.js
@@ -1,7 +1,6 @@
 const getSharedStorage = require('./lib/SharedDBStorageService.js').getSharedStorage;
 const DSUService = require("./lib/DSUService");
-const { Roles, messagesEnum, shipment, FoldersEnum } = require('./constants');
-const shipmentStatusesEnum = shipment.shipmentStatusesEnum;
+const { Roles, messagesEnum, FoldersEnum } = require('./constants');
 const NotificationsService = require('./lib/NotificationService.js');
 const CommunicationService = require('./lib/CommunicationService.js');
 
@@ -44,21 +43,17 @@ class ShipmentsService extends DSUService {
 
   // -> Functions for creation of shipment
   async createShipment(data) {
-    
-    const { statusDsu } = await this.createShipmentOtherDSUs();
-
-    const status = await this.updateStatusDsu(shipmentStatusesEnum.InPreparation, statusDsu.uid);
 
     const shipmentModel = {
-      sponsorId: data.sponsorId,
-      targetCmoId: data.targetCmoId,
-      studyId: data.studyId,
-      orderId: data.orderId,
-      siteId: data.siteId,
-      siteRegionId: data.siteRegionId,
-      siteCountry: data.siteCountry,
+      sponsorId: data.sponsor_id,
+      targetCmoId: data.target_cmo_id,
+      studyId: data.study_id,
+      orderId: data.order_id,
+      siteId: data.site_id,
+      siteRegionId: data.site_region_id,
+      siteCountry: data.site_country,
       temperatures: data.keep_between_temperature,
-      temperature_comments: data.temperatures,
+      temperature_comments: data.temperature_comments,
       requestDate: data.requestDate,
       deliveryDate: data.delivery_date,
       lastModified: data.lastModified,
@@ -70,8 +65,7 @@ class ShipmentsService extends DSUService {
       {
       ...shipmentModel,
       shipmentSSI: shipment.uid,
-      status: status.history,
-      statusSSI: status.uid,
+
       },
       shipment.uid
     );
@@ -81,7 +75,6 @@ class ShipmentsService extends DSUService {
       messagesEnum.ShipmentInPreparation,
       {
         shipmentSSI: shipment.uid,
-        statusKeySSI: statusDsu.uid,
       },
       messagesEnum.ShipmentInPreparation
     );
@@ -91,37 +84,11 @@ class ShipmentsService extends DSUService {
       messagesEnum.ShipmentInPreparation,
       {
         shipmentSSI: shipment.uid,
-        statusKeySSI: statusDsu.uid,
       },
       messagesEnum.ShipmentInPreparation
     );
 
     return shipmentDb;
-  }
-
-  async createShipmentOtherDSUs() {
-
-    const statusDsu = await this.saveEntityAsync(
-      {
-        history: [],
-      },
-      FoldersEnum.ShipmentsStatuses
-    );
-
-    return { statusDsu };
-
-  }
-
-  async updateStatusDsu(newStatus, keySSI) {
-    const statusDsu = await this.getEntityAsync(keySSI, FoldersEnum.ShipmentsStatuses);
-    const result = await this.updateEntityAsync(
-      {
-        ...statusDsu,
-        history: [...statusDsu.history, { status: newStatus, date: new Date().getTime() }],
-      },
-      FoldersEnum.ShipmentsStatuses
-    );
-    return result;
   }
 
   // -> Functions for mounting newly created shipment in other actors except cmo
@@ -130,7 +97,7 @@ class ShipmentsService extends DSUService {
     switch (role) {
       case Roles.Sponsor:
         shipment = await this.mountEntityAsync(shipmentSSI, FoldersEnum.Shipments);
-        status = await this.mountEntityAsync(statusKeySSI, FoldersEnum.ShipmentsStatuses);
+        status = await this.mountEntityAsync(statusKeySSI, FoldersEnum.Statuses);
 
         shipmentDb = await this.addShipmentToDB(
           {
@@ -145,7 +112,7 @@ class ShipmentsService extends DSUService {
         return shipmentDb;
       case Roles.Site:
         shipment = await this.mountEntityAsync(shipmentSSI, FoldersEnum.Shipments);
-        status = await this.mountEntityAsync(statusKeySSI, FoldersEnum.ShipmentsStatuses);
+        status = await this.mountEntityAsync(statusKeySSI, FoldersEnum.Statuses);
 
         shipmentDb = await this.addShipmentToDB(
           {
