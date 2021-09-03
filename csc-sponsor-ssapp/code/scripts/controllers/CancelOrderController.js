635325bf63f258eb96d8d87f3624345ae1452988
mastaleru <mastaleru.rafael@gmail.com>
2021-09-03 14:32:18 +0300

    Small fix for #176

--- csc-sponsor-ssapp/code/scripts/controllers/CancelOrderController.js
+++ csc-sponsor-ssapp/code/scripts/controllers/CancelOrderController.js
@@ -4,7 +4,7 @@ export default class CancelOrderController extends WebcController {
   constructor(...props) {
     super(...props);
     this.model.onChange('cancelOrderModal.comment.value', () => {
-      this.model.cancelOrderModal.commentIsEmpty = this.model.cancelOrderModal.comment.value.length === 0;
+      this.model.cancelOrderModal.commentIsEmpty = this.model.cancelOrderModal.comment.value.trim() === "";
     });
 
   }
