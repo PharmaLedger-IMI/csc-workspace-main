e5fdd5fe3488086c8b73af067cd78323d6b42dc4
mastaleru <mastaleru.rafael@gmail.com>
2021-09-28 16:29:27 +0300

    Code improvements for #306

--- csc-courier-ssapp/code/scripts/controllers/CourierSingleShipmentController.js
+++ csc-courier-ssapp/code/scripts/controllers/CourierSingleShipmentController.js
@@ -32,27 +32,26 @@ class CourierSingleShipmentController extends ViewShipmentBaseController {
   }
 
   setShipmentActions(shipment) {
-    const actions = {};
+    const actions = {
+      canPickupShipment:false,
+      canEditShipment:false,
+      canAddMessage:false,
+      canDeliverShipment:false
+    };
 
-    if (shipment.status[0].status === shipmentStatusesEnum.ReadyForDispatch) {
-      actions.canScanPickShipment = true;
-      actions.canEditShipment = false;
-      actions.canAddMessage = false;
-      actions.scan = false;
-    } else if (shipment.status[0].status === shipmentStatusesEnum.InTransit) {
-      if (shipment.shipmentBilling) {
-        actions.canEditShipment = false;
-        actions.canScanPickShipment = false;
-        actions.canAddMessage = true;
-        actions.scan = true;
-      } else {
-        actions.canEditShipment = true;
-        actions.canScanPickShipment = false;
-        actions.canAddMessage = false;
-        actions.scan = false;
-      }
+    switch (shipment.status[0].status) {
+      case shipmentStatusesEnum.ReadyForDispatch:
+        actions.canPickupShipment = true;
+        break;
+      case shipmentStatusesEnum.InTransit:
+        if (shipment.shipmentBilling) {
+          actions.canAddMessage = true;
+          actions.canDeliverShipment = true;
+        } else {
+          actions.canEditShipment = true;
+        }
+        break;
     }
-
     return actions;
   }
 
