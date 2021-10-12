432c8eda265fb54941bd8f2de72e06fd88b1e9ff
mastaleru <mastaleru.rafael@gmail.com>
2021-10-12 10:05:09 +0300

    cleanup

--- csc-courier-ssapp/code/scripts/controllers/ShipmentsController.js
+++ csc-courier-ssapp/code/scripts/controllers/ShipmentsController.js
@@ -1,17 +1,7 @@
 const { BaseShipmentsController } = WebCardinal.controllers;
-
 const cscServices = require('csc-services');
-
-const eventBusService = cscServices.EventBusService;
-const momentService = cscServices.momentService;
-const ShipmentService = cscServices.ShipmentService;
-
-const { Topics, Commons, Roles } = cscServices.constants;
+const {Roles } = cscServices.constants;
 const {
-  shipmentStatusesEnum,
-  shipmentCMOTableHeaders,
-  shipmentSiteTableHeaders,
-  shipmentSponsorTableHeaders,
   shipmentCourierTableHeaders
 } = cscServices.constants.shipment;
 
@@ -19,11 +9,8 @@ const {
 export default class ShipmentsController extends BaseShipmentsController {
 
   constructor(...props) {
-
     let role = Roles.Courier;
-
     super(...props);
-
     this.role = role;
   }
 
