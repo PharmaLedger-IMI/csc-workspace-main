64708e7419d1a7f5fee4a22f22da5e02ca4fdee2
mastaleru <mastaleru.rafael@gmail.com>
2021-10-15 17:16:24 +0300

    #417

--- csc-services/services/constants/folders.js
+++ csc-services/services/constants/folders.js
@@ -4,6 +4,7 @@ module.exports.FoldersEnum = {
     Comments: '/comments',
     Statuses: '/statuses',
     Kits: '/kits',
+    StudyKits:"/study-kits",
     Shipments: '/shipments',
     ShipmentsStatuses: '/shipmentsstatuses',
     ShipmentTransitBilling: '/shipmenttransitbilling',
