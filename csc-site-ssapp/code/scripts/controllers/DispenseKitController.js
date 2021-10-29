c6bbbe514539a44c3b3c15824476703120a40261
Aris Papaprodromou <arispapapro@gmail.com>
2021-10-29 10:15:02 +0300

    Implemented #487

--- csc-site-ssapp/code/scripts/controllers/DispenseKitController.js
+++ csc-site-ssapp/code/scripts/controllers/DispenseKitController.js
@@ -2,6 +2,7 @@ const { WebcController } = WebCardinal.controllers;
 const cscServices = require('csc-services');
 const KitsService = cscServices.KitsService;
 const viewModelResolver = cscServices.viewModelResolver;
+const { kitsStatusesEnum } = cscServices.constants.kit;
 
 class DispenseKitController extends WebcController {
 
@@ -9,22 +10,45 @@ class DispenseKitController extends WebcController {
     super(...props);
     this.kitsService = new KitsService(this.DSUStorage);
     this.initViewModel();
-
-    this.onTagClick("dispense-kit",()=>{
-      //TODO #487
-      console.log("Implement #487")
-    })
+    this.attachEvents();
   }
 
   async initViewModel() {
     const model = {
       kitModel: viewModelResolver('kit')
     };
-      let { studyId, orderId } = this.history.location.state.kit;
+      let { studyId, orderId, keySSI } = this.history.location.state.kit;
       this.model.studyId = studyId;
       this.model.orderId = orderId;
+      this.model.kitSSI = keySSI;
 
     this.model = model;
+  }
+
+   attachEvents(){
+     this.dispenseKitHandler();
+  }
+
+   dispenseKitHandler(){
+    this.onTagClick("dispense-kit",  async () => {
+      const kitData = this.getkitData();
+       await this.kitsService.updateKit(this.model.kitSSI, kitsStatusesEnum.Dispensed, kitData);
+        this.navigateToPageTag('kit', {
+          keySSI: this.model.kitSSI
+        });
+    });
+  }
+
+  getkitData() {
+    return {
+      patientId: this.model.kitModel.form.patientId.value,
+      doseType: this.model.kitModel.form.doseType.value,
+      doseVolume: this.model.kitModel.form.doseVolume.value,
+      visitId: this.model.kitModel.form.visitId.value,
+      dispensingPartyId: this.model.kitModel.form.dispensingPartyId.value,
+      receivedDate: this.model.kitModel.form.receivedDate.value,
+      receivedTime: this.model.kitModel.form.receivedTime.value,
+    }
   }
 
 }
