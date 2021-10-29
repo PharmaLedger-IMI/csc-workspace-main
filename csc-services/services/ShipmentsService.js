e27445c831d21dff819593d451c3a5d580547687
abhayprasadnovartis <abhay.prasad_ext@novartis.com>
2021-10-29 11:11:54 +0530

    Corrected Requested Delivery Datetime

--- csc-services/services/ShipmentsService.js
+++ csc-services/services/ShipmentsService.js
@@ -47,6 +47,7 @@ class ShipmentsService extends DSUService {
 		const shipmentModel = {
 			orderSSI: data.orderSSI,
 			requestDate: data.requestDate,
+			deliveryDate: data.delivery_date,
 			orderId: data.orderId,
 			sponsorId: data.sponsorId,
 			shipmentId: data.orderId,
