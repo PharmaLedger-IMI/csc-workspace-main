b9ad7c7d0bb391c534477569f68f626a517ad6bd
mastaleru <mastaleru.rafael@gmail.com>
2022-06-03 14:16:14 +0300

    #728

--- bin/clean.js
+++ bin/clean.js
@@ -8,7 +8,7 @@ function  walk(dir, filterFiles, filterFolders, done) {
 
     let recWalk = function(dir, filterFiles, filterFolders, done) {
         fs.readdir(dir, function(err, list) {
-            if (err) return done(err, result);
+            if (err) return done(err, results);
             let i = 0;
             (function next() {
                 let file = list[i++];
@@ -39,13 +39,21 @@ function  walk(dir, filterFiles, filterFolders, done) {
 
 
 
-function filterFiles(name){
-
-    if(name.endsWith("cardinal\\seed") || name.endsWith("cardinal/seed")){
-        console.log("Deleting seed file:", name);
-        fs.unlinkSync(name);
+function filterFiles(name) {
+    if (name.endsWith("/seed") || name.endsWith("\\seed")) {
+        let readSSI;
+        try {
+            readSSI = fs.readFileSync(name);
+            readSSI = readSSI.toString();
+        } catch (e) {
+            return console.log(e);
+        }
+        if (!readSSI.includes("ssi:alias")) {
+            console.log("Deleting seed file:", name);
+            fs.unlinkSync(name);
+        }
+        return undefined;
     }
-    return undefined;
 }
 
 
@@ -61,19 +69,30 @@ function deleteFolderContent(name, text){
     );
 }
 
-function filterFolders(name){
-    if(name){
-        if(name.endsWith("/anchors") || name.endsWith("\\anchors")){
+function filterFolders(name) {
+    const bricksStorage = 'brick-storage';
+    const mqsFolder = 'mqs';
+    if (name) {
+        if (name.endsWith("/anchors") || name.endsWith("\\anchors")) {
             deleteFolderContent(name, "Deleting anchors:")
         }
 
-        // if(name.endsWith("/bundles") || name.endsWith("\\bundles")){
-        //     deleteFolderContent(name, "Deleting bundles:")
-        // }
-
-        if(name.indexOf("/brick-storage/") > 0 || name.indexOf("\\brick-storage\\") > 0 ){
+        if (name.indexOf(`/${bricksStorage}/`) > 0 || name.indexOf(`\\${bricksStorage}\\`) > 0) {
             deleteFolderContent(name, "Deleting bricks:")
         }
+
+        if (name.indexOf(`/${mqsFolder}/`) > 0 || name.indexOf(`\\${mqsFolder}\\`) > 0) {
+            fs.readdir(name, function (err, queueList) {
+                if (err) {
+                    console.log(err);
+                }
+                queueList.forEach(queue => {
+                    deleteFolderContent(path.join(name, queue), "Deleting mqs...")
+                })
+            })
+
+        }
+
     }
     return undefined;
 }
@@ -85,3 +104,4 @@ walk(myPath, filterFiles, filterFolders, function(err, result){
     })
 
 });
+
