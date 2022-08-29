16c6265ff302e93a6ddd7254e76e856a9078a967
paiuscatalin <p.cata95@gmail.com>
2022-08-29 15:56:53 +0300

    wip - demiurge integration

---
+++ trust-loader-config/demiurge-wallet/loader/environment.js
@@ -0,0 +1,15 @@
+export default {
+  "appName": "Demiurge",
+  "vault": "server",
+  "agent": "browser",
+  "system":   "any",
+  "browser":  "any",
+  "mode":  "dev-secure",
+  "vaultDomain":  "vault",
+  "didDomain":  "vault",
+  "enclaveType":"WalletDBEnclave",
+  // "disabledFeatures": "add-group",
+  "sw": false,
+  "pwa": false,
+  "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure) sw:(true, false) pwa:(true, false)"
+}
