c82f3d306c21e2e5f2d16ede306b5d5d1783b279
paiuscatalin <p.cata95@gmail.com>
2022-10-03 15:48:20 +0300

    Created configuration to Demiurge's environment.js to disable menu items

--- trust-loader-config/demiurge-wallet/loader/environment.js
+++ trust-loader-config/demiurge-wallet/loader/environment.js
@@ -8,7 +8,8 @@ export default {
   "vaultDomain":  "vault",
   "didDomain":  "vault",
   "enclaveType":"WalletDBEnclave",
-  // "disabledFeatures": "add-group",
+  "disabledFeatures": "add-group",
+  "hiddenMenuItems": ["Governance", "Audit"],
   "sw": false,
   "pwa": false,
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure) sw:(true, false) pwa:(true, false)"
