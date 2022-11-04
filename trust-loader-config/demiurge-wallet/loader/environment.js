dcf157e4f1987cb4032494f28cc99d62c69ae912
paiuscatalin <p.cata95@gmail.com>
2022-11-04 11:00:38 +0200

    Revert "Implement upload/download candidate documentation file for fixed structure voting session"
    This reverts commit a29e17bd92e0e4f34c80aa4a87af351fa44d40a0.

--- trust-loader-config/demiurge-wallet/loader/environment.js
+++ trust-loader-config/demiurge-wallet/loader/environment.js
@@ -9,7 +9,7 @@ export default {
   "didDomain":  "vault",
   "enclaveType":"WalletDBEnclave",
   "disabledFeatures": "add-group",
-  "enableGovernance": true,
+  "enableGovernance": false,
   "sw": false,
   "pwa": false,
   "legenda for properties": " vault:(server, browser) agent:(mobile,  browser)  system:(iOS, Android, any) browser:(Chrome, Firefox, any) mode:(autologin,dev-autologin, secure, dev-secure) sw:(true, false) pwa:(true, false)"
